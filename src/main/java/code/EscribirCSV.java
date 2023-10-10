4376f105a101f1127ec899ac7e3bc1a011a3df0c
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-10-10 16:50:03 +0200

    1. Creacion del archivo csv con los datos que escriba el usuario a traves del objeto empleado

--- src/main/java/code/EscribirCSV.java
+++ src/main/java/code/EscribirCSV.java
@@ -1,2 +1,57 @@
-package code;public class EscribirCSV {
+package code;
+
+import javaBeans.Empleado;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class EscribirCSV {
+
+    public static void pedirEmpleados(Path p) {
+
+        Path filePath = Path.of("target/empleados.csv");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+        String respuesta;
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(p.toFile(), true))) {
+
+            Scanner sc = new Scanner(System.in);
+            do {
+                System.out.print("Nombre del empleado: ");
+                String nombre = sc.nextLine();
+
+                System.out.print("Sueldo del empleado: ");
+                double sueldo = Double.parseDouble(sc.nextLine());
+
+                System.out.print("Año de nacimiento del empleado: ");
+                Date añoNacimiento = dateFormat.parse(sc.nextLine());
+
+                System.out.print("Antigüedad del empleado: ");
+                Date antiguedad = dateFormat.parse(sc.nextLine());
+
+                Empleado empleado = new Empleado(nombre, sueldo, añoNacimiento, antiguedad);
+
+                // Escribir los datos del empleado en el archivo CSV
+                writer.write(empleado.getNombre() + "," +
+                        empleado.getSueldo() + "," +
+                        dateFormat.format(empleado.getAñoNacimiento()) + "," +
+                        empleado.getAntiguedad() + "\n");
+
+                System.out.print("¿Quieres añadir otro empleado? (Si/No): ");
+                respuesta = sc.nextLine().toLowerCase();
+            } while (respuesta.equals("si"));
+
+            System.out.println("Datos de empleados guardados en empleados.csv.");
+        } catch (IOException e) {
+            System.err.println("Error al escribir en el archivo CSV: " + e.getMessage());
+        } catch (ParseException e) {
+            System.err.println("Error al parsear una de las fechas: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/javaBeans/Empleado.java
+++ src/main/java/javaBeans/Empleado.java
@@ -1,2 +1,59 @@
-package javaBeans;public class Empleado {
+package javaBeans;
+
+import java.util.Date;
+
+public class Empleado {
+
+    private String nombre;
+    private Double sueldo;
+    private Date añoNacimiento, antiguedad;
+
+    public Empleado(String nombre, Double sueldo, Date añoNacimiento, Date antiguedad) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.añoNacimiento = añoNacimiento;
+        this.antiguedad = antiguedad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public Date getAñoNacimiento() {
+        return añoNacimiento;
+    }
+
+    public void setAñoNacimiento(Date añoNacimiento) {
+        this.añoNacimiento = añoNacimiento;
+    }
+
+    public Date getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setAntiguedad(Date antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "nombre='" + nombre + '\'' +
+                ", sueldo=" + sueldo +
+                ", añoNacimiento=" + añoNacimiento +
+                ", antiguedad=" + antiguedad +
+                '}';
+    }
 }
